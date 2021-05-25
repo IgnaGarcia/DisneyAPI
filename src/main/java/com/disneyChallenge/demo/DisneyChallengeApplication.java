66db98a9e70f2d6a3af3c810b6f91f099f1fe1ff
Igna Garcia <gnachoxp@gmail.com>
2021-05-25 19:37:48 -0300

    init

---
+++ src/main/java/com/disneyChallenge/demo/DisneyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.disneyChallenge.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyChallengeApplication.class, args);
+	}
+
+}
