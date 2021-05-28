81b076fa80860e8aafcaf23f57adf0fb6e45af50
Igna Garcia <gnachoxp@gmail.com>
2021-05-27 23:46:29 -0300

    add character controller and service

--- src/main/java/com/disneyChallenge/demo/controllers/CharacterController.java
+++ src/main/java/com/disneyChallenge/demo/controllers/CharacterController.java
@@ -3,19 +3,52 @@ package com.disneyChallenge.demo.controllers;
 import com.disneyChallenge.demo.models.Character;
 import com.disneyChallenge.demo.services.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/character")
+@RequestMapping(path = "/api/characters")
 public class CharacterController {
     private final CharacterService characterService;
 
     @Autowired
-    public CharacterController(CharacterService characterService) { this.characterService = characterService; }
+    public CharacterController(CharacterService characterService) {
+        this.characterService = characterService;
+    }
 
     @GetMapping
-    public List<Character> getCharacters(){ return characterService.getCharacters(); }
+    public List<Character> getCharacters(@RequestParam(required = false) String name,
+                                         @RequestParam(required = false) Integer age,
+                                         @RequestParam(required = false) Long movie) {
+        return name != null
+                ? characterService.getCharacterByName(name)
+                : age != null
+                ? characterService.getCharacterByAge(age)
+                : movie != null
+                ? characterService.getCharacterByMovie(movie)
+                : characterService.getCharacters();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Character getCharacter(@PathVariable("id") Long id) {
+        return characterService.getCharacter(id);
+    }
+
+    @PostMapping
+    public Character createCharacter(@RequestBody Character character) {
+        return characterService.createCharacter(character);
+    }
+
+    @PutMapping
+    public Character updateCharacter(@RequestBody Character character) {
+        return characterService.updateCharacter(character);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteCharacter(@PathVariable("id") Long id) {
+        return characterService.deleteCharacter(id)
+                ? "Character " + id + " deleted succesfully"
+                : "ERROR to delete Character " + id;
+    }
 }
--- src/main/java/com/disneyChallenge/demo/services/CharacterService.java
+++ src/main/java/com/disneyChallenge/demo/services/CharacterService.java
@@ -1,15 +1,59 @@
 package com.disneyChallenge.demo.services;
 
 import com.disneyChallenge.demo.models.Character;
+import com.disneyChallenge.demo.repositories.CharacterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CharacterService {
+    private final CharacterRepository characterRepository;
+
+    @Autowired
+    public CharacterService(CharacterRepository characterRepository) {
+        this.characterRepository = characterRepository;
+    }
 
     public List<Character> getCharacters() {
-        return List.of(
-                new Character()
-        );
+        return characterRepository.findAll();
+    }
+
+    public Character getCharacter(Long id) {
+        Optional<Character> character = characterRepository.findById(id);
+        return (character != null && character.isPresent())
+                ? character.get()
+                : null;
+    }
+
+    public List<Character> getCharacterByName(String name) {
+        return characterRepository.findByNameLike(name);
+    }
+
+    public List<Character> getCharacterByAge(Integer age) {
+        return characterRepository.findByAge(age);
+    }
+
+    public List<Character> getCharacterByMovie(Long movie) {
+        return null;//characterRepository.findByMovies(movie);
+    }
+
+    public Character createCharacter(Character character) {
+        return (character != null && character.getId() == null)
+                ? characterRepository.save(character)
+                : null;
+    }
+
+    public Character updateCharacter(Character character) {
+        return (character != null && getCharacter(character.getId()) != null)
+                ? characterRepository.save(character)
+                : null;
+    }
+
+    public Boolean deleteCharacter(Long id) {
+        if (getCharacter(id) != null) characterRepository.deleteById(id);
+        return getCharacter(id) == null;
     }
 }
