29ea8d85506878916896e141d1a7c773a7d7e4d8
Igna Garcia <gnachoxp@gmail.com>
2021-05-27 23:46:40 -0300

    add genre controller and service

--- src/main/java/com/disneyChallenge/demo/controllers/GenreController.java
+++ src/main/java/com/disneyChallenge/demo/controllers/GenreController.java
@@ -6,16 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/genre")
+@RequestMapping(path = "/api/genres")
 public class GenreController {
     private final GenreService genreService;
 
     @Autowired
-    public GenreController(GenreService genreService) { this.genreService = genreService; }
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
 
     @GetMapping
-    public List<Genre> getGenres(){ return genreService.getGenres(); }
+    public List<Genre> getGenres() {
+        return genreService.getGenres();
+    }
 }
--- src/main/java/com/disneyChallenge/demo/services/GenreService.java
+++ src/main/java/com/disneyChallenge/demo/services/GenreService.java
@@ -1,15 +1,22 @@
 package com.disneyChallenge.demo.services;
 
 import com.disneyChallenge.demo.models.Genre;
+import com.disneyChallenge.demo.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class GenreService {
+    private final GenreRepository genreRepository;
+
+    @Autowired
+    public GenreService(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
 
     public List<Genre> getGenres() {
-        return List.of(
-                new Genre()
-        );
+        return genreRepository.findAll();
     }
 }
