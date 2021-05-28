62723aa6d0841bf3450bc9ac9acc276f81935eee
Igna Garcia <gnachoxp@gmail.com>
2021-05-27 23:45:58 -0300

    .

--- src/main/java/com/disneyChallenge/demo/models/Genre.java
+++ src/main/java/com/disneyChallenge/demo/models/Genre.java
@@ -1,45 +1,62 @@
 package com.disneyChallenge.demo.models;
 
-import com.disneyChallenge.demo.models.Film;
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "genres")
 public class Genre {
-    private Integer id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String image;
-    private List<Film> filmList;
+    @OneToMany(mappedBy = "g_id", cascade = {
+            CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.PERSIST,
+            CascadeType.REFRESH})
+    private List<Movie> movieList;
 
-    public Genre() { }
+    public Genre() {
+    }
 
-    public Genre(Integer id,
+    public Genre(Long id,
                  String name,
-                 String image,
-                 List<Film> filmList) {
+                 String image) {
         this.id = id;
         this.name = name;
         this.image = image;
-        this.filmList = filmList;
     }
 
     public Genre(String name,
-                 String image,
-                 List<Film> filmList) {
+                 String image) {
         this.name = name;
         this.image = image;
-        this.filmList = filmList;
     }
 
-    public Integer getId() { return id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public void setName(String name) { this.name = name; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getImage() { return image; }
+    public String getImage() {
+        return image;
+    }
 
-    public void setImage(String image) { this.image = image; }
+    public void setImage(String image) {
+        this.image = image;
+    }
 
-    public List<Film> getFilmList() { return filmList; }
-
-    public void setFilmList(List<Film> filmList) { this.filmList = filmList; }
+    public List<Movie> getMovieList() {
+        return movieList;
+    }
 }
