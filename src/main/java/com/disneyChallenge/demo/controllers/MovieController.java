1827d4d4bd67aa500dedd26b50426047ec44183b
Igna Garcia <gnachoxp@gmail.com>
2021-05-27 23:46:51 -0300

    add movie controller and service

---
+++ src/main/java/com/disneyChallenge/demo/controllers/MovieController.java
@@ -0,0 +1,52 @@
+package com.disneyChallenge.demo.controllers;
+
+import com.disneyChallenge.demo.models.Movie;
+import com.disneyChallenge.demo.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/movies")
+public class MovieController {
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getMovies(@RequestParam(required = false) String name,
+                                 @RequestParam(required = false) Long genre,
+                                 @RequestParam(required = false) String order) {
+        return name != null
+                ? movieService.getMovieByTitle(name, order)
+                : genre != null
+                ? movieService.getMovieByGenre(genre, order)
+                : movieService.getMovies(order);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Movie getCharacter(@PathVariable("id") Long id) {
+        return movieService.getMovie(id);
+    }
+
+    @PostMapping
+    public Movie createCharacter(@RequestBody Movie movie) {
+        return movieService.createMovie(movie);
+    }
+
+    @PutMapping
+    public Movie updateCharacter(@RequestBody Movie movie) {
+        return movieService.updateMovie(movie);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteMovie(@PathVariable("id") Long id) {
+        return movieService.deleteMovie(id)
+                ? "Movie "+id+" deleted succesfully"
+                : "ERROR to delete Movie "+id;
+    }
+}
---
+++ src/main/java/com/disneyChallenge/demo/services/MovieService.java
@@ -0,0 +1,70 @@
+package com.disneyChallenge.demo.services;
+
+import com.disneyChallenge.demo.models.Movie;
+import com.disneyChallenge.demo.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> getMovies(String order) {
+        return (order == "ASC")
+                ? movieRepository.findAllOrderByDateAsc()
+                : (order == "DESC")
+                ? movieRepository.findAllOrderByDateDesc()
+                : movieRepository.findAll();
+    }
+
+    public Movie getMovie(Long id) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        return (movie != null && movie.isPresent())
+                ? movie.get()
+                : null;
+    }
+
+    public List<Movie> getMovieByTitle(String title, String order) {
+        return (order == "ASC")
+                ? movieRepository.findByTitleLikeOrderByDateAsc(title)
+                : (order == "DESC")
+                ? movieRepository.findByTitleLikeOrderByDateDesc(title)
+                : movieRepository.findByTitleLike(title);
+    }
+
+    public List<Movie> getMovieByGenre(Long genre, String order) {
+        return null;
+        /*
+        return (order == "ASC")
+                ? movieRepository.findByGenreOrderByDateAsc(genre)
+                : (order == "DESC")
+                ? movieRepository.findByGenreOrderByDateDesc(genre)
+                : movieRepository.findByGenre(genre);
+         */
+    }
+
+    public Movie createMovie(Movie movie) {
+        return (movie != null && movie.getId() == null)
+                ? movieRepository.save(movie)
+                : null;
+    }
+
+    public Movie updateMovie(Movie movie) {
+        return (movie != null && getMovie(movie.getId()) != null)
+                ? movieRepository.save(movie)
+                : null;
+    }
+
+    public Boolean deleteMovie(Long id) {
+        if (getMovie(id) != null) movieRepository.deleteById(id);
+        return getMovie(id) == null;
+    }
+}
